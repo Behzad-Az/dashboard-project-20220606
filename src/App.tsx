4383cd07a2baccda7b6a64d29849039964ee23b6
Behzad-Az <behy1368@gmail.com>
2022-06-06 12:29:21 -0700

    created src file structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
+
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <h1 className="text-3xl underline">
-      Hello world!
+      App
     </h1>
   );
 }
