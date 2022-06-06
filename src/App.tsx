4247cff0137f3c1338b04a9c836538e0b2976bc8
Behzad-Az <behy1368@gmail.com>
2022-06-06 13:16:44 -0700

    working on sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,47 @@
-import React from 'react';
+import React, { useEffect, FC } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { FiSettings } from 'react-icons/fi';
+import { TooltipComponent } from '@syncfusion/ej2-react-popups';
 
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+interface Props {};
+
+const App: FC<Props> = () : JSX.Element => {
+  const activeMenu: boolean = false;
   return (
-    <h1 className="text-3xl underline">
-      App
-    </h1>
+    <div>
+      <BrowserRouter>
+        <div className='flex relative dark:bg-main-dark-bg'>
+          <div className='fixed right-4 bottom-4' style={{ zIndex: '1000' }}>
+            <TooltipComponent content='Settings' position='TopCenter'>
+              <button 
+                type='button' 
+                className='text-3xl p-3 hover:drop-shadow-xl hover:bg-light-gray text-white'
+                style={{ 
+                  background: 'blue',
+                  borderRadius: '50%'
+                }}
+              >
+                <FiSettings />
+              </button>
+            </TooltipComponent>
+          </div>
+          {
+            activeMenu ? (
+              <div className='w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white'>
+                Sidebar
+              </div>
+            ) : (
+              <div className='w-0 dark:bg-secondary-dark-bg'>
+                Sidebar
+              </div>
+            )
+          }
+        </div>
+      </BrowserRouter>
+    </div>
   );
-}
+};
 
 export default App;
