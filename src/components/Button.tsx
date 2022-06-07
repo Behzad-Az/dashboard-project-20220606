26a24140a47b490709680e45212b38eaabbd5b36
Behzad-Az <behy1368@gmail.com>
2022-06-06 18:25:20 -0700

    creating the ecommerce page

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,10 +1,26 @@
 import React, { FC } from 'react';
 
-interface Props {};
+interface Props {
+  color: string;
+  backgroundColor: string;
+  text: string;
+  borderRadius: string;
+  size: string;
+};
 
-const Button: FC<Props> = () : JSX.Element => {
+const Button: FC<Props> = ({ color, backgroundColor, text, borderRadius, size }) : JSX.Element => {
   return (
-    <div>Button</div>
+    <button
+      type='button'
+      style={{
+        backgroundColor,
+        color,
+        borderRadius
+      }}
+      className={`text-${size} p-3 hover:drop-shadow-xl`}
+    >
+      {text}
+    </button>
   );
 };
 
