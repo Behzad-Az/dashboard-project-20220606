a6cd3777dbc2ea10246d4201ca90ea3ac0f0a909
Behzad-Az <behy1368@gmail.com>
2022-06-06 18:06:01 -0700

    refactor

--- src/App.tsx
+++ src/App.tsx
@@ -32,7 +32,8 @@ import './App.css';
 
 interface Props {};
 
-const renderSideBar = (activeMenu: boolean) : JSX.Element => {
+const DynamicSideBar: FC<Props> = () : JSX.Element => {
+  const { activeMenu } = useAppContext();
   return activeMenu ? 
   (
     <div className='w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white'>
@@ -65,41 +66,42 @@ const App: FC<Props> = () : JSX.Element => {
               </button>
             </TooltipComponent>
           </div>
-          { renderSideBar(activeMenu) }
+
+          <DynamicSideBar />
+
           <div className={`dark:bg-main-bg bg-main-bg min-h-screen w-full ${activeMenu ? 'md:ml-72' : 'flex-2' }`}>
             <div className='fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full'>
               <Navbar />
             </div>
-          </div>
+            <div>
+              <Routes>
+                {/* Dashboard */}
+                <Route path='/' element={<Ecommerce />} />
+                <Route path='/ecommerce' element={<Ecommerce />} />
 
-          <div>
-            <Routes>
-              {/* Dashboard */}
-              <Route path='/' element={<Ecommerce />} />
-              <Route path='/ecommerce' element={<Ecommerce />} />
+                {/* Pages */}
+                <Route path='/orders' element={<Orders />} />
+                <Route path='/employees' element={<Employees />} />
+                <Route path='/customers' element={<Customers />} />
 
-              {/* Pages */}
-              <Route path='/orders' element={<Orders />} />
-              <Route path='/employees' element={<Employees />} />
-              <Route path='/customers' element={<Customers />} />
+                {/* Apps */}
+                <Route path='/kanban' element={<Kanban />} />
+                <Route path='/editor' element={<Editor />} />
+                <Route path='/calendar' element={<Calendar />} />
+                <Route path='/color-picker' element={<ColorPicker />} />
 
-              {/* Apps */}
-              <Route path='/kanban' element={<Kanban />} />
-              <Route path='/editor' element={<Editor />} />
-              <Route path='/calendar' element={<Calendar />} />
-              <Route path='/color-picker' element={<ColorPicker />} />
+                {/* Charts */}
+                <Route path='/line' element={<LineChart />} />
+                <Route path='/area' element={<AreaChart />} />
+                <Route path='/bar' element={<BarChart />} />
+                <Route path='/pie' element={<PieChart />} />
+                <Route path='/financial' element={<FinancialChart />} />
+                <Route path='/color-mapping' element={<ColorMapping />} />
+                <Route path='/pyramid' element={<PyramidChart />} />
+                <Route path='/stacked' element={<StackedChart />} />
 
-              {/* Charts */}
-              <Route path='/line' element={<LineChart />} />
-              <Route path='/area' element={<AreaChart />} />
-              <Route path='/bar' element={<BarChart />} />
-              <Route path='/pie' element={<PieChart />} />
-              <Route path='/financial' element={<FinancialChart />} />
-              <Route path='/color-mapping' element={<ColorMapping />} />
-              <Route path='/pyramid' element={<PyramidChart />} />
-              <Route path='/stacked' element={<StackedChart />} />
-
-            </Routes>
+              </Routes>
+            </div>
           </div>
 
         </div>
--- src/pages/Ecommerce.tsx
+++ src/pages/Ecommerce.tsx
@@ -1,10 +1,22 @@
 import React, { FC } from 'react';
+import { BsCurrencyDollar } from 'react-icons/bs';
+import { GoPrimitiveDot } from 'react-icons/go';
+
+import { StackedChart, PieChart, Button, SparkLineChart } from '../components';
+import { earningData, SparklineAreaData, ecomPieChartData } from '../data/dummy';
+import { useAppContext } from '../contexts/ContextProvider';
 
 interface Props {};
 
 const Ecommerce: FC<Props> = () : JSX.Element => {
   return (
-    <div>Ecommerce</div>
+    <div className='mt-12'>
+      <div className='flex flex-wrap lg:flex-nowrap justify-center'>
+        <div className='bg-white dark:text-gray-200 dark:bg-secondary-dark-bg h-44 rounded-xl w-full lg:w-80 p-8 pt-9 m-3 bg-hero-pattern bg-no-repeat bg-cover bg-center'>
+
+        </div>
+      </div>
+    </div>
   );
 };
 
