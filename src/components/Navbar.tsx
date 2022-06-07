1d42043d922fc9768eddf2daacaf1af75fe8a9dd
Behzad-Az <behy1368@gmail.com>
2022-06-06 17:19:37 -0700

    working on navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,97 @@
-import React, { FC } from 'react';
+import React, { FC, useEffect } from 'react';
+import { AiOutlineMenu } from 'react-icons/ai';
+import { FiShoppingCart } from 'react-icons/fi';
+import { BsChatLeft } from 'react-icons/bs';
+import { RiNotification3Line } from 'react-icons/ri';
+import { MdKeyboardArrowDown } from 'react-icons/md';
+import { TooltipComponent } from '@syncfusion/ej2-react-popups';
 
-interface Props {};
+import avatar from '../data/avatar.jpg';
+import { Cart, Chat, Notification, UserProfile } from '.';
+import { useAppContext } from '../contexts/ContextProvider';
 
-const Navbar: FC<Props> = () : JSX.Element => {
+interface NavbarProps {};
+interface NavButtonProps {
+  title: string;
+  customFunc: () => void;
+  icon: JSX.Element;
+  color: string;
+  dotColor?: string;
+};
+
+const NavButton: FC<NavButtonProps> = ({ title, customFunc, icon, color, dotColor }) : JSX.Element => (
+  <TooltipComponent content={title} position='BottomCenter'>
+    <button
+      type='button'
+      onClick={customFunc}
+      style={{ color }}
+      className='relative text-xl rounded-full p-3 hover:bg-light-gray'
+    >
+      <span 
+        style={{ background: dotColor }}
+        className='absolute inline-flex rounded-full h-2 w-2 right-2 top-2'
+      >
+        {icon}
+      </span>
+    </button>
+  </TooltipComponent>
+);
+
+const Navbar: FC<NavbarProps> = () : JSX.Element => {
+  const { activeMenu, setActiveMenu, isClicked, setIsClicked, handleClick } = useAppContext();
   return (
-    <div>Navbar</div>
+    <div className='flex justify-between p-2 md:mx-6 relative'>
+      <NavButton 
+        title='Menu' 
+        customFunc={() => setActiveMenu(prevState => !prevState)}
+        color='blue'
+        icon={<AiOutlineMenu />}
+      />
+      <div className='flex'>
+        <NavButton 
+          title='Cart' 
+          customFunc={() => handleClick('cart')}
+          color='blue'
+          icon={<FiShoppingCart />}
+        />
+        <NavButton 
+          title='Chat' 
+          customFunc={() => handleClick('chat')}
+          color='blue'
+          dotColor='#03C9D7'
+          icon={<BsChatLeft />}
+        />
+        <NavButton 
+          title='Notifications' 
+          customFunc={() => handleClick('notification')}
+          color='blue'
+          dotColor='#03C9D7'
+          icon={<RiNotification3Line />}
+        />
+      </div>
+      <TooltipComponent
+        content='profile'
+        position='BottomCenter'
+      >
+        <div 
+          className='flex items-center gap-2 cursor-pointer p-1 hover:bg-light-gray rounded-lg'
+          onClick={() => handleClick('userProfile')}
+        >
+          <img className='rounded-full w-8 h-8' src={avatar} />
+          <p>
+            <span className='text-gray-400 text-14'>Hi, </span> {' '}
+            <span className='text-gray-400 font-bold ml-1 text-14'>Michael</span>
+          </p>
+          <MdKeyboardArrowDown className='text-gray-400 text-14' />
+        </div>
+        
+      </TooltipComponent>
+
+      { isClicked.cart && <Cart /> }
+      { isClicked.chat && <Chat /> }
+      { isClicked.notification && <Notification /> }
+      { isClicked.userProfile && <UserProfile /> }
+    </div>
   );
 };
 
--- src/contexts/ContextProvider.tsx
+++ src/contexts/ContextProvider.tsx
@@ -1,30 +1,51 @@
 import React, { createContext, useContext, useState, FC, Dispatch, SetStateAction } from 'react';
 
-interface AppContextInterface {
-  activeMenu: boolean,
-  setActiveMenu: Dispatch<SetStateAction<boolean>>
-};
-
 interface ContextProviderInterface {
   children: React.ReactNode;
 };
 
-const initialState = {
+interface IsClickedInterface {
+  chat: boolean;
+  cart: boolean;
+  userProfile: boolean;
+  notification: boolean;
+};
+
+type ClickedButton = 'chat' | 'cart' | 'userProfile' | 'notification';
+interface AppContextInterface {
+  activeMenu: boolean;
+  setActiveMenu: Dispatch<SetStateAction<boolean>>;
+  isClicked: IsClickedInterface;
+  setIsClicked: Dispatch<SetStateAction<IsClickedInterface>>;
+  handleClick: (clicked: ClickedButton) => void | null;
+};
+
+const initialState: IsClickedInterface = {
   chat: false,
   cart: false,
   userProfile: false,
   notification: false
 };
 
-const AppContext = createContext<AppContextInterface>({ activeMenu: false, setActiveMenu: () => {} });
+const AppContext = createContext<AppContextInterface>({ activeMenu: false, setActiveMenu: () => {}, isClicked: initialState, setIsClicked: () => {}, handleClick: () => {} });
 
 export const ContextProvider: FC<ContextProviderInterface> = ({ children }) => {
   const [activeMenu, setActiveMenu] = useState<boolean>(true);
+  const [isClicked, setIsClicked] = useState<IsClickedInterface>(initialState);
+  const handleClick = (clicked: string) : void => {
+    setIsClicked({
+      ...initialState,
+      [clicked]: true
+    });
+  };
   return (
     <AppContext.Provider
       value={{
         activeMenu,
-        setActiveMenu
+        setActiveMenu,
+        isClicked,
+        setIsClicked,
+        handleClick
       }}
     >
       {children}
